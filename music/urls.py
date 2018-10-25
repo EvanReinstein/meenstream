545f1ca5178645fdac28a3b398cbd305376143fa
Mark Mandell <markman0922@gmail.com>
2018-10-25 15:39:53 -0700

    adds playlist view page

--- music/urls.py
+++ music/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.playlist_index, name='playlist_index'),
+    path('<int:playlist_id>', views.playlist_view, name='playlist_view'),
 ]
--- music/views.py
+++ music/views.py
@@ -5,3 +5,7 @@ from .models import Playlist, Profile
 def playlist_index(request):
     playlists = Playlist.objects.all()
     return render(request, 'music/playlist_index.html', {'playlists': playlists})
+
+def playlist_view(request, playlist_id):
+    playlist = Playlist.objects.get(id=playlist_id)
+    return render(request, 'music/playlist_view.html', {'playlist': playlist})
