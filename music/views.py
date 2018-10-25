abc87dfe8388a19b2665445f2197068df265308f
Mark Mandell <markman0922@gmail.com>
2018-10-25 14:39:55 -0700

    Adds pages, music, and accounts models/functionality.  Sets up login/register/logout functionality.  Templates for accounts, pages, and music.

--- music/views.py
+++ music/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Playlist, Profile
 
 # Create your views here.
-def playlist_list(request):
+def playlist_index(request):
     playlists = Playlist.objects.all()
-    return render(request, 'music/playlist_list.html', {'playlists': playlists})
+    return render(request, 'music/playlist_index.html', {'playlists': playlists})
