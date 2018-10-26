8421ce99f16824e56d7b433f0e99f485f0952bf8
Mark Mandell <markman0922@gmail.com>
2018-10-26 11:49:10 -0700

    adds heroku config

--- music_app/settings.py
+++ music_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '48_ap2(74$-a#fc$xbqsk_(#x6lx046$9^3d-^9-7iz#h^s5@g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -124,3 +126,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'music_app/static')
 ]
+
+django_heroku.settings(locals())
