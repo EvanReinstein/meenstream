835edcd4746d869b4c2f346b67614803a2c27cc0
Mark Mandell <markman0922@gmail.com>
2018-10-29 15:16:07 -0700

    adds fix for images/ login redirect

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,7 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('profile') ##Double check this functionality/correctness
+            return redirect('home') ##Double check this functionality/correctness
         else:
             return render(request, 'accounts/login.html', {'error': 'Invalid credentials'})
 
