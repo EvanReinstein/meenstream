98ff7a75c47b4348d5f16b66c773dc3f8c692508
EvanReinstein <edreins@gmail.com>
2018-10-29 11:42:58 -0700

    finishes backend form validation and adds email validator to Profile model

--- music/models.py
+++ music/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import validate_email
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -14,5 +14,5 @@ class Profile(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile")
     playlist_id = models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name="profile")
     full_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, validators=[validate_email])
     photo = models.CharField(max_length=100)
