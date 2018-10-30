ad7b39e5e269290ccae347db2cf5b2338f253d23
Mark Mandell <markman0922@gmail.com>
2018-10-30 16:39:44 -0700

    adds heroku settings

--- music_app/settings.py
+++ music_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '48_ap2(74$-a#fc$xbqsk_(#x6lx046$9^3d-^9-7iz#h^s5@g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
