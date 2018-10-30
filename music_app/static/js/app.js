645ce6bd1238fb558aa21ab0a07263dec20ddb10
Natasha Quijano <niquijanodesign@gmail.com>
2018-10-30 16:20:50 -0700

    nav bar js functionality

--- music_app/static/js/app.js
+++ music_app/static/js/app.js
@@ -1,11 +1,26 @@
 console.log('hey everyone!')
 
 
-$(document).ready(function() {
-  $('body').on('click', function() {
+$(document).ready(function () {
+  $('body').on('click', function () {
     console.log('clicccccck')
   })
-
-
-
 });
+
+
+// Nav bar functionality 
+
+const hamburger = document.querySelector('.hamburger-icon');
+
+// Hamburger menu click event listener
+
+hamburger.addEventListener('click', handleMenuClick);
+
+// Handle menu click
+
+function handleMenuClick() {
+  var sidebar = document.querySelector('.side-bar')
+  if (sidebar.classList.contains('.hidden')) {
+
+  }
+}
